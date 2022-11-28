50177803e178ed73f5eb6a560146bf44faefc563
Santiago Zappa <santiagozappab@gmail.com>
2022-11-28 16:21:33 -0500

    deploy2

--- server.js
+++ server.js
@@ -12,11 +12,7 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 //middleware
-app.use(cors(
-    {
-        origin: ['http://localhost:3000']
-    },
-))
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
